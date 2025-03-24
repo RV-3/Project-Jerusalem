a9cb27762d4f40089907852b78d22d8548360661
tomhanks <tom@tomhankss-MacBook-Air.local>
2025-03-24 12:18:01 +0100

    update blocked slots

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -13,7 +13,7 @@ const client = sanityClient({
   dataset: 'production',
   useCdn: false,
   apiVersion: '2023-01-01',
-  token: 'skLXmnuhIUZNJQF7cGeN77COiIcZRnyj7ssiWNzdveN3S0cZF6LTw0uvznBO4l2VoolGM5nSVPYnw13YZtrBDEohI3fJWa49gWWMp0fyOX5tP1hxp7qrR9zDHxZoivk0n7yUa7pcxqsGvzJ0Z2bKVbl29i3QuaIBtHoOqGxiN0SvUwgvO9W8'
+  token: process.env.REACT_APP_SANITY_TOKEN || 'skLXmnuhIUZNJQF7cGeN77COiIcZRnyj7ssiWNzdveN3S0cZF6LTw0uvznBO4l2VoolGM5nSVPYnw13YZtrBDEohI3fJWa49gWWMp0fyOX5tP1hxp7qrR9zDHxZoivk0n7yUa7pcxqsGvzJ0Z2bKVbl29i3QuaIBtHoOqGxiN0SvUwgvO9W8'
 })
 
 export default function Calendar() {
