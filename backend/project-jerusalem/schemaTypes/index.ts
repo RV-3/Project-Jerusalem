a4a0df9c443d3f5c67a7b510aad0d130cd363496
tomhanks <tom@tomhankss-MacBook-Air.local>
2025-04-14 09:52:11 +0300

    added chapel nickname feature

--- backend/project-jerusalem/schemaTypes/index.ts
+++ backend/project-jerusalem/schemaTypes/index.ts
@@ -1,3 +1,17 @@
 import reservation from './reservation'
 
-export const schemaTypes = [reservation]
+// Import other schemas
+import chapel from '../../schemas/chapel.mjs'
+import calendarPassword from '../../schemas/calendarPassword.mjs'
+import autoBlockedHours from '../../schemas/autoBlockedHours.mjs'
+import autoBlockedDays from '../../schemas/autoBlockedDays.mjs'
+import blocked from '../../schemas/blocked.mjs'
+
+export const schemaTypes = [
+  reservation,
+  chapel,
+  calendarPassword,
+  autoBlockedHours,
+  autoBlockedDays,
+  blocked
+]
